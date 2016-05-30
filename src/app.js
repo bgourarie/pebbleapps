ca08da30c9d7f5c3603d9e47f81737523461da90
bgourarie <bgourarie@users.noreply.github.com>
2016-05-29 17:10:48 -0700

    enhanced extended forecast and add refresh option
    shake to refresh, and select a forecast time to see extra info (wind, pressure, temp)

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@
 var UI = require('ui');
 var ajax = require('ajax');
 var Vector2 = require('vector2');
+var Accel = require('ui/accel');
+var Vibe = require('ui/vibe');
 
 var splashWindow = new UI.Window();
 
@@ -28,11 +30,16 @@ splashWindow.show();
 });
 // card.show();
 */
+var getFarTemp = function(owm_data_temp){
+  return Math.round((owm_data_temp - 273.15) * 9/5 + 32 );
+};
 var parseFeed =  function(data, quantity) {
   var items = [];
   for(var i=0; i< quantity; i++) {
     var title = data.list[i].weather[0].main;
     title = title.charAt(0).toUpperCase() + title.substring(1);
+    title += ' '+getFarTemp(data.list[i].main.temp) + 'F';
+    
     var time = data.list[i].dt_txt;
     time = time.substring(time.indexOf('-')+1, time.indexOf(':') + 3);
     items.push({
@@ -41,14 +48,15 @@ var parseFeed =  function(data, quantity) {
     });
   }
   return items;
-}
+};
+
 // fetch teh weather info
 
 var lat = 47.620805;
 var long =  -122.349725;
 var myAPIKey = 'da5ab015f853c0537b10646cfdee92a1';
 var URL = 'http://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+long +'&appid='+myAPIKey;
-var URL2 = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long +'&appid='+myAPIKey;
+// var URL2 = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long +'&appid='+myAPIKey;
 
 // make the req:
 ajax(
@@ -62,15 +70,45 @@ ajax(
     // just check they extracted okay..
     for(var i =0; i<menuItems.length; i++){
       console.log(menuItems[i].title+ ' | '+menuItems[i].subtitle);
-  }   
-  var resultsMenu = new UI.Menu({
-    sections: [{
-      title: 'Current Forecast',
-      items: menuItems
-    }]
-  });
+    }   
+    var resultsMenu = new UI.Menu({
+      sections: [{
+        title: 'Current Forecast',
+        items: menuItems
+      }]
+    });
+    resultsMenu.on('select',function(e){
+      var forecast = data.list[e.itemIndex];
+      var content = data.list[e.itemIndex].weather[0].description;
+      content = content.charAt(0).toUpperCase()+ content.substring(1);
+      
+      content += '\nTemperature ' + getFarTemp(forecast.main.temp) + '˚F' + '\nPressure: '+ Math.round(forecast.main.pressure) + ' mbar ' + '\nWind: '+ Math.round(forecast.wind.speed) + ' mph, ' + Math.round(forecast.wind.deg) + '˚';
+      var detailCard = new UI.Card({
+        title: 'Details',
+        subtitle:e.item.subtitle,
+        body: content
+      });
+      detailCard.show();
+    });
   
     resultsMenu.show();
+    resultsMenu.on('accelTap', function(e){
+      console.log('TAP!');
+      console.log( 'axis: '+ e.axis + ' , direction '+e.direction);
+      ajax({
+            url: URL,
+            type:'json'
+          },
+          function(data){
+            var newItems = parseFeed(data, 10);
+            resultsMenu.items(0,newItems);
+            Vibe.vibrate('short');
+          },
+           function(error){
+
+           }
+          );
+    });
     splashWindow.hide();
   /*  
     console.log('Successfully fetched the weaher');
@@ -87,3 +125,4 @@ ajax(
     console.log('Failed fetching weather data ' + err);
   }
 );
+Accel.init();
